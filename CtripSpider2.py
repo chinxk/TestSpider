831b38a72d5e38817a3bda6d243bee5c86ab8c1b
chinxk <chinxk@gmail.com>
2018-04-17 18:04:47 +0800

    can't get info from qunar.com excepted in DEBUG mode

--- CtripSpider2.py
+++ CtripSpider2.py
@@ -1,16 +1,31 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
 
 def get_json():
-    browser = webdriver.Firefox()
-    browser.get("https://www.qunar.com/")
-    input_str = browser.find_element_by_id('q')
-    input_str.clear()
-    input_str.send_keys("MakBook pro")
-    button = browser.find_element_by_class_name('btn-search')
-    button.click()
+    # dcap = dict(DesiredCapabilities.PHANTOMJS)
+
+    # 构造一个PHANTOMJS对象
+    phan = dict(DesiredCapabilities.PHANTOMJS)
+    # 禁止加载图片
+    # phan["phantomjs.page.settings.loadImages"] = False
+
+    # 禁用缓存
+    # phan["phantomjs.page.settings.disk-cache"] = True
+
+    phan["phantomjs.page.settings.userAgent"] = ("Mozilla/4.0 (compatible; MSIE 5.5; windows NT)")
+
+    phan["phantomjs.page.settings.resourceTimeout"] = 50000
+
+    # 加载自定义配置
+    browser = webdriver.PhantomJS(desired_capabilities=phan)
+    browser.implicitly_wait(40000)
+
+    browser.get("https://flight.qunar.com/site/interroundtrip_compare.htm?fromCity=%E6%88%90%E9%83%BD&toCity=%E9%A9%AC%E5%BE%B7%E9%87%8C&fromDate=2018-04-30&toDate=2018-05-07&fromCode=CTU&toCode=MAD&from=qunarindex&lowestPrice=null&isInter=true&favoriteKey=&showTotalPr=null&adultNum=1&childNum=0&cabinClass=")
+    time.sleep(60)
     print(browser.page_source)
+    browser.close()
 
-
+    browser.quit()
 if __name__ == '__main__':
     get_json()
